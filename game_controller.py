6d80526e152d04ee96c3ca224d0416c5344f0c4d
Omprakash Saini <op.fb21@gmail.com>
2022-09-25 14:18:19 +0900

    Test

--- game_controller.py
+++ game_controller.py
@@ -5,4 +5,6 @@ class game_controller:
         while(True):
             piece,cell=players[current_player].make_move()
             #players[current_player].move(player,cell,board,)
-            current_player=(current_player+1)%len(players)
+               current_player=(current_player+1)%len(players)
+
+PRINT("HI")
